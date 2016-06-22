d21a40b6d466cf830ce0d0fbc2e73c348835e53e
lijingchn <lijingjing.chn@gmail.com>
2016-06-22 12:20:42 +0800

    all

--- myfootprint/settings.py
+++ myfootprint/settings.py
@@ -125,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+                '/home/toy_project/footprint_proj/myfootprint/static/',
+                )
